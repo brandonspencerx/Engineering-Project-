93cbcf705e37a090394036092df604764d489f92
brandonspencerx <brandonspencer019@gmail.com>
2025-11-06 12:47:52 -0500

    remapped notes to sensors

--- python_engineering_sensors2.py
+++ python_engineering_sensors2.py
@@ -25,33 +25,33 @@ print("Listening for Arduino input...")
 octave_up = False
 
 sensors_dict = { # maps each sensor to a note
-                0:'A', 
-                1:'As',
-                2:'B',
-                3:'C',
-                4:'Cs',
-                5:'D',
-                6:'Ds',
-                7:'E',
-                8:'F',
-                9:'Fs',
-                10:'G',
-                11:'Gs',
+                0:'C', 
+                1:'Cs',
+                2:'D',
+                3:'Ds',
+                4:'E',
+                5:'F',
+                6:'Fs',
+                7:'G',
+                8:'Gs',
+                9:'A',
+                10:'As',
+                11:'B',
                 }
 
 octave_up_dict = { # octave up versions for each sensor
-                0:'A4', 
-                1:'As4',
-                2:'B4',
-                3:'C4',
-                4:'Cs4',
-                5:'D4',
-                6:'Ds4',
-                7:'E4',
-                8:'F4',
-                9:'Fs4',
-                10:'G4',
-                11:'Gs4'
+                0:'C4', 
+                1:'Cs4',
+                2:'D4',
+                3:'Ds4',
+                4:'Es4',
+                5:'F4',
+                6:'Fs4',
+                7:'G4',
+                8:'Gs4',
+                9:'A4',
+                10:'As4',
+                11:'B4'
                 }
 
 # play sound corresponding to the triggered piezo, allowing overlap.
@@ -87,7 +87,7 @@ try:
 
                 if sensor_index == 12: # sensor 12 for octave switch
 
-                    if octave_up == False: 
+                    if octave_up == False:  
                         octave_up = True
                         print("Octave up)")
                         print(f"Impact detected on Piezo {sensor_index}")

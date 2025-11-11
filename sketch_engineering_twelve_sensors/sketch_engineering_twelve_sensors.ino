0eb2710a32151ed99652244b51a51c5b1504a5b8
brandonspencerx <brandonspencer019@gmail.com>
2025-11-11 11:37:56 -0500

    added arduino sketch for twelve sensors

---
+++ sketch_engineering_twelve_sensors/sketch_engineering_twelve_sensors.ino
@@ -0,0 +1,35 @@
+const int piezoPins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12};
+const int numPiezos = sizeof(piezoPins) / sizeof(piezoPins[0]);
+const int threshold = {750};
+const int debounceDelay = 2000;
+
+unsigned long lastImpactTime[numPiezos];
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < numPiezos; i++) {
+    lastImpactTime[i] = 0;
+  }
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  for (int i = 0; i < numPiezos; i++) {
+    analogRead(piezoPins[i]); // Dummy read for ADC settling
+    int sensorReading = analogRead(piezoPins[i]);
+
+    if (sensorReading >= threshold) {
+      if (currentTime - lastImpactTime[i] > debounceDelay) {
+        //Serial.print("Impact detected on Piezo ");
+        //Serial.print(i);
+        //Serial.print(" with reading: ");
+        //Serial.println(sensorReading);
+        Serial.print("PIEZO_");
+        Serial.println(i);
+        Serial.println(" ");
+        lastImpactTime[i] = currentTime;
+      }
+    }
+  }
+}
